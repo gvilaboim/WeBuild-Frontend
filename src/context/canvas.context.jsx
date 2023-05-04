493dd13685e63a2efef87dc59b02ef7fe6d52f33
gferreira7 <104030930+gferreira7@users.noreply.github.com>
2023-05-04 15:39:06 +0100

    finishing split sections

--- src/context/canvas.context.jsx
+++ src/context/canvas.context.jsx
@@ -22,7 +22,7 @@ function CanvasProviderWrapper(props) {
   const saveChanges = async (id, siteData) => {
     try {
       const response = await canvasStoreService.saveChanges(id, siteData);
-      console.log(response.data.sections)
+      console.log(response.data.sections[2])
       return response.data;
     } catch (error) {
       console.log(error);
